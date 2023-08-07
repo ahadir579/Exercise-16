09e785e38dbc77d88ed6eda7020dd699f08e7418
ahadir579 <139805536+ahadir579@users.noreply.github.com>
2023-08-08 01:07:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+"use strict";
+/*More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+• Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
+
+• Add one new guest to the beginning of your array.
+
+• Add one new guest to the middle of your array. • Use append() to add one new guest to the end of your list. • Print a new set of invitation messages, one for each person in your list.*/
+let guestList = [
+    "Zia Khan",
+    "Jahanzaib Tayab",
+    "Abid Shirwani"
+];
+// set 1 informing people that you found a bigger dinner table.
+for (let guest of guestList) {
+    console.log(`Hello, ${guest}, we found a bigger dinner table.`);
+}
+// step 2 add a new guest to the beginningof Array.
+guestList.unshift("Ali Phull");
+// console.log(guestList)
+// set 3 add one guest to the middle of array.
+let middleIndex = Math.floor(guestList.length / 2);
+guestList.splice(middleIndex, 0, "Mian Asif");
+// console.log(guestList)
+// 4th step use append() to add new guest to the end of your list.
+guestList.push("Usama Phull");
+// console.log(guestList)
+// Final Step Print a new set of invitation messages, one for each person in your list.
+console.log("New set of invitation messages:");
+for (let i of guestList) {
+    console.log(`Dear ${i} you are invited to dinner.`);
+}
